35254e7e685ddf53d8d190947f18a1c68a68a8aa
eylemugurel <eylemugurel@hotmail.com>
2024-10-22 08:12:28 +0200

    Add DataHelper class

---
+++ DataHelper.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types=1);
+/**
+ * DataHelper.php
+ *
+ * (C) 2024 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+namespace TestToolkit;
+
+/**
+ * Provides reusable data sets for PHPUnit, including common value types like
+ * non-strings, non-integers, booleans, and combinations of test data (Cartesian
+ * product).
+ *
+ * @codeCoverageIgnore
+ */
+class DataHelper
+{
+    /**
+     * Generates the Cartesian product of multiple arrays.
+     *
+     * This function is designed to calculate the cartesian product of multiple
+     * arrays, which is particularly useful in PHPUnit data provider functions.
+     * It is ideal for generating all possible combinations of test data from
+     * multiple sets. The cartesian product operation results in an array of
+     * arrays, each containing a unique combination of elements from the input
+     * arrays.
+     *
+     * Example usage in PHPUnit data provider:
+     * ```php
+     * public function dataProvider() {
+     *     return DataHelper::Cartesian([1, 2, 3], ['a', 'b']);
+     * }
+     *
+     * // Returns: [1, 'a'], [1, 'b'], [2, 'a'], [2, 'b'], [3, 'a'], [3, 'b']
+     * ```
+     *
+     * @param array $arrays
+     *   The input arrays for which the Cartesian product will be calculated.
+     * @return array
+     *   The Cartesian product as an array of arrays, representing all
+     *   combinations.
+     *
+     * @link https://stackoverflow.com/a/15973172
+     *   Sergiy Sokolenko's answer on StackOverflow
+     */
+    public static function Cartesian(array ...$arrays): array
+    {
+        $result = [[]];
+        foreach ($arrays as $key => $values) {
+            $append = [];
+            foreach ($result as $product) {
+                foreach ($values as $item) {
+                    $product[$key] = $item;
+                    $append[] = $product;
+                }
+            }
+            $result = $append;
+        }
+        return $result;
+    }
+}
